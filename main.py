861ba04fec26bc7e19c0cad1bec8684f9ae946b7
HPorada <HPorada@users.noreply.github.com>
2021-12-16 18:08:43 +0100

    Functions file added, example of NetworkX graph visualisation

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
 import socket
 
-from graphviz import Digraph
+import functions as f
 
+from graphviz import Digraph
 from arango import ArangoClient
+import networkx as nx
+import matplotlib.pyplot as plt
 
 client = ArangoClient(hosts='http://localhost:8529')
 
@@ -17,6 +20,9 @@ db_Nov = client.db('AmyloidsNov', username='root', password='Amyloids')
 
 s = socket.socket()
 s.settimeout(100)
+
+
+# ArangoDatabase.ClientSetting.HttpRequestTimeout = TimeSpan.FromMilliseconds(Timeout.Infinite)
 
 # print(db.graph('ExtendedWithEdges'))
 # graph = db.graph('ExtendedWithEdges')
@@ -127,30 +133,62 @@ def search_by_questions(database):
         json.dump(inter, outfile)
 
 
-# check_questions_simple(db_Sep, "Faster aggregation", "Yes; implied by kinetics.", "No information")
+f.check_questions_simple(db_Sep, "Faster aggregation", "Yes; implied by kinetics.", "No information")
+
+
 # search_for_fragment(db_Sep, 'DAEFRHDSGY')
 # search_for_key_word(db_Sep, 'pH')
 # search_for_all_connected(db_Sep, 'amyloids/IAPP')
 # search_for_most_common(db_Sep, 10, 'interactor')
 
-search_by_questions(db_Sep)
+# search_by_questions(db_Sep)
 
-with open("./test.json", "r") as file:
-    arango_graph = json.load(file)
+def graphviz_graph(filename):
+    with open(filename, "r") as file:
+        arango_graph = json.load(file)
 
-graph_name = 'example_graph'
+    graph_name = 'example_graph_2'
 
-g = Digraph(graph_name, filename=graph_name, format='jpeg', engine='neato')
-g.attr(scale='2', label='Searching with starting node', fontsize='18')
-g.attr('node', shape='circle', fixedsize='false', width='0.5')
+    g = Digraph(graph_name, filename=graph_name, format='jpeg', engine='neato')
+    g.attr(scale='2', label='Searching with starting node', fontsize='18')
+    g.attr('node', shape='circle', fixedsize='false', width='0.5')
 
-for item in arango_graph:
-    for vertex in item['vertices']:
-        g.node(vertex['_id'], label=vertex['_key'])
-    for edge in item['edges']:
-        g.edge(edge['_from'], edge['_to'])
+    for item in arango_graph:
+        for vertex in item['vertices']:
+            g.node(vertex['_id'], label=vertex['_key'])
+        for edge in item['edges']:
+            g.edge(edge['_from'], edge['_to'])
 
-# Render to file into some directory
-# g.render(directory='/tmp/', filename=graph_name)
-# Or just show rendered file using system default program
-g.view()
+    # Render to file into some directory
+    # g.render(directory='/tmp/', filename=graph_name)
+    # Or just show rendered file using system default program
+    g.view()
+
+
+def networkx_graph(filename):
+    with open(filename) as file:
+        json_data = json.loads(file.read())
+
+    G = nx.DiGraph()
+
+    for i in range(len(json_data)):
+        G.add_nodes_from(
+            (elem['_id'])
+            for elem in json_data[i]['vertices']
+        )
+
+        G.add_edges_from(
+            (elem['_from'], elem['_to'])
+            for elem in json_data[i]['edges']
+        )
+
+    nx.draw(
+        G,
+        with_labels=True
+    )
+
+    plt.show()
+
+
+graphviz_graph('test.json')
+networkx_graph('test.json')
