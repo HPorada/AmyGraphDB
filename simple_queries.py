ef7be3e08977040c82ade9ad11ff0c09b4b3cc1d
HPorada <HPorada@users.noreply.github.com>
2021-12-19 22:27:27 +0100

    Simple_filter function

--- simple_queries.py
+++ simple_queries.py
@@ -1,11 +1,16 @@
 import json
 
 
-def check_questions_simple(database, q1, q2, q3, filename="result"):
+def filter_simple(database, q1, q2, q3, filename="result"):
     aql = database.aql
 
     cursor = database.aql.execute(
-        'FOR int IN interactionsE FILTER int.question_1 == @q1 FILTER int.question_2 == @q2 FILTER int.question_3 == @q3 RETURN int ',
+        """for item in sequences
+            for v, e, p in 1..1 any item._id graph "Simple"
+                filter e.question_1 == @q1
+                filter e.question_2 == @q2
+                filter e.question_3 == @q3
+                return p""",
         bind_vars={'q1': q1, 'q2': q2, 'q3': q3}
     )
 
@@ -13,10 +18,10 @@ def check_questions_simple(database, q1, q2, q3, filename="result"):
 
     inter = [doc for doc in cursor]
 
-    # for x in inter:
-    #     print(x)
+    for x in inter:
+        print(x)
 
-    with open(f"./json_data/{filename}.json", "w") as outfile:
-        json.dump(inter, outfile)
+    # with open(f"./json_data/{filename}.json", "w") as outfile:
+    #     json.dump(inter, outfile)
 
 
