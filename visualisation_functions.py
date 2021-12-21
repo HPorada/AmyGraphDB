d3f76d14d44081f99f6e9e65e44c5df406827d33
HPorada <HPorada@users.noreply.github.com>
2021-12-21 15:41:41 +0100

    Graphviz - different colours and shapes of interaction nodes, depending on answers to questions

--- visualisation_functions.py
+++ visualisation_functions.py
@@ -2,6 +2,7 @@ import json
 import networkx as nx
 import matplotlib.pyplot as plt
 from graphviz import Digraph
+import re
 
 
 def graphviz_graph(filename):
@@ -12,11 +13,31 @@ def graphviz_graph(filename):
 
     g = Digraph(graph_name, filename=f".\graphs\{graph_name}", format='jpeg', engine='neato')
     g.attr(scale='2', label='Searching with starting node', fontsize='18')
-    g.attr('node', shape='circle', fixedsize='false', width='0.5')
+    g.attr('node', shape='rectangle', style='filled', fillcolor='', fixedsize='false', width='0.5')
 
     for item in arango_graph:
         for vertex in item['vertices']:
-            g.node(vertex['_id'], label=vertex['_key'])
+
+            if re.search("^sequencesE", vertex['_id']) is not None:
+                # g.attr('node', shape='triangle')
+                g.node(vertex['_id'], label=vertex['name'] if 'name' in vertex else "seq:" + vertex['_key'])
+
+            elif re.search("^interactionsE", vertex['_id']) is not None:
+
+                shape = question1_shape(vertex['question_1'])
+                color = question2_color(vertex['question_2'])
+
+                g.attr('node', shape=shape, style='filled', fillcolor=color)
+                g.node(vertex['_id'], label="int:" + vertex['_key'])
+                g.attr('node', shape='rectangle', fillcolor='', fixedsize='false', width='0.5')
+
+            elif re.search("^amyloidsE", vertex['_id']) is not None:
+                g.attr('node', shape='ellipse')
+                g.node(vertex['_id'], label=vertex['name'])
+                g.attr('node', shape='rectangle', fillcolor='', fixedsize='false', width='0.5')
+
+            else:
+                g.node(vertex['_id'], label=vertex['name'] if 'name' in vertex else vertex['_key'])
         for edge in item['edges']:
             g.edge(edge['_from'], edge['_to'])
 
@@ -27,15 +48,37 @@ def graphviz_graph(filename):
     g.view()
 
 
+def question1_shape(answer):
+    switch = {
+        "Faster aggregation": "triangle",
+        "Slower aggregation": "invtriangle",
+        "No aggregation": "octagon",
+        "No effect": "diamond",
+        "No information": "box"
+    }
+    return switch.get(answer, "box")
+
+
+def question2_color(answer):
+    switch = {
+        "Yes, direct evidence.": "darkgreen",
+        "Yes; implied by kinetics.": "forestgreen",
+        "Formation of fibrils by the interactee is inhibited": "red1",
+        "No": "yellow2",
+        "No information": ""
+    }
+    return switch.get(answer, "")
+
+
 def networkx_graph(filename):
-    with open(f".\json_data\{filename}") as file:
+    with open(f'.\json_data\{filename}') as file:
         json_data = json.loads(file.read())
 
     G = nx.DiGraph()
 
     for i in range(len(json_data)):
         G.add_nodes_from(
-            (elem['_id'])
+            (elem['_key'])
             for elem in json_data[i]['vertices']
         )
 
@@ -49,7 +92,31 @@ def networkx_graph(filename):
         with_labels=True
     )
 
-    #nx.write_graphml_lxml(G, f"{filename}.gml")
-    nx.write_gexf(G, f"{filename}.gexf")
+    # forceatlas2 = ForceAtlas2(
+    #     # Behavior alternatives
+    #     outboundAttractionDistribution=True,  # Dissuade hubs
+    #     linLogMode=False,  # NOT IMPLEMENTED
+    #     adjustSizes=False,  # Prevent overlap (NOT IMPLEMENTED)
+    #     edgeWeightInfluence=1.0,
+    #
+    #     # Performance
+    #     jitterTolerance=1.0,  # Tolerance
+    #     barnesHutOptimize=True,
+    #     barnesHutTheta=1.2,
+    #     multiThreaded=False,  # NOT IMPLEMENTED
+    #
+    #     # Tuning
+    #     scalingRatio=2.0,
+    #     strongGravityMode=False,
+    #     gravity=1.0,
+    #
+    #     # Log
+    #     verbose=True)
+    #
+    # positions = forceatlas2.forceatlas2_networkx_layout(G, pos=None, iterations=2000)
+
+    # nx.write_graphml_lxml(G, f"{filename}.gml")
+    # nx.write_gml(G, f"{filename}.graphml")
+    # nx.write_gexf(G, f"{filename}.gexf")
 
     plt.show()
