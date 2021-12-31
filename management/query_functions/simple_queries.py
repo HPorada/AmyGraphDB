aece932effd008acf042694133e71b652c2ed25f
HPorada <HPorada@users.noreply.github.com>
2021-12-31 14:54:15 +0100

    Filter_questions functions corrected

--- management/query_functions/simple_queries.py
+++ management/query_functions/simple_queries.py
@@ -2,15 +2,99 @@ import json
 
 
 def filter_questions_simple(database, q1, q2, q3, filename="result", directory=None):
-    cursor = database.aql.execute(
-        """for item in sequences
-            for v, e, p in 1..1 any item._id graph "Simple"
-                filter e.question_1 == @q1
-                filter e.question_2 == @q2
-                filter e.question_3 == @q3
-                return p""",
-        bind_vars={'q1': q1, 'q2': q2, 'q3': q3}
-    )
+    if (
+            q1 == "Faster aggregation" or q1 == "Slower aggregation" or q1 == "No aggregation" or q1 == "No effect" or q1 == "No information") and (
+            q2 == "Yes, direct evidence." or q2 == "Yes; implied by kinetics." or q2 == "Formation of fibrils by the interactee is inhibited" or q2 == "No" or q2 == "No information") and (
+            q3 == "Yes" or q3 == "No" or q3 == "No information"):
+
+        cursor = database.aql.execute(
+            """for item in sequences
+                   for v, e, p in 1..1 any item._id graph "Simple"
+                       filter e.question_1 == @q1
+                       filter e.question_2 == @q2
+                       filter e.question_3 == @q3
+                       return p""",
+            bind_vars={'q1': q1, 'q2': q2, 'q3': q3}
+        )
+
+    elif (
+            q1 == "Faster aggregation" or q1 == "Slower aggregation" or q1 == "No aggregation" or q1 == "No effect" or q1 == "No information") and (
+            q2 == "Yes, direct evidence." or q2 == "Yes; implied by kinetics." or q2 == "Formation of fibrils by the interactee is inhibited" or q2 == "No" or q2 == "No information"):
+
+        cursor = database.aql.execute(
+            """for item in sequences
+                   for v, e, p in 1..1 any item._id graph "Simple"
+                       filter e.question_1 == @q1
+                       filter e.question_2 == @q2
+                       return p""",
+            bind_vars={'q1': q1, 'q2': q2}
+        )
+
+    elif (
+            q2 == "Yes, direct evidence." or q2 == "Yes; implied by kinetics." or q2 == "Formation of fibrils by the interactee is inhibited" or q2 == "No" or q2 == "No information") and (
+            q3 == "Yes" or q3 == "No" or q3 == "No information"):
+
+        cursor = database.aql.execute(
+            """for item in sequences
+                   for v, e, p in 1..1 any item._id graph "Simple"
+                       filter e.question_2 == @q2
+                       filter e.question_3 == @q3
+                       return p""",
+            bind_vars={'q2': q2, 'q3': q3}
+        )
+
+    elif (
+            q1 == "Faster aggregation" or q1 == "Slower aggregation" or q1 == "No aggregation" or q1 == "No effect" or q1 == "No information") and (
+            q3 == "Yes" or q3 == "No" or q3 == "No information"):
+
+        cursor = database.aql.execute(
+            """for item in sequences
+                   for v, e, p in 1..1 any item._id graph "Simple"
+                       filter e.question_1 == @q1
+                       filter e.question_3 == @q3
+                       return p""",
+            bind_vars={'q1': q1, 'q3': q3}
+        )
+
+    elif (
+            q1 == "Faster aggregation" or q1 == "Slower aggregation" or q1 == "No aggregation" or q1 == "No effect" or q1 == "No information"):
+
+        cursor = database.aql.execute(
+            """for item in sequences
+                   for v, e, p in 1..1 any item._id graph "Simple"
+                       filter e.question_1 == @q1
+                       return p""",
+            bind_vars={'q1': q1}
+        )
+
+    elif (
+            q2 == "Yes, direct evidence." or q2 == "Yes; implied by kinetics." or q2 == "Formation of fibrils by the interactee is inhibited" or q2 == "No" or q2 == "No information"):
+
+        cursor = database.aql.execute(
+            """for item in sequences
+                   for v, e, p in 1..1 any item._id graph "Simple"
+                       filter e.question_2 == @q2
+                       return p""",
+            bind_vars={'q2': q2}
+        )
+
+    elif (
+            q3 == "Yes" or q3 == "No" or q3 == "No information"):
+
+        cursor = database.aql.execute(
+            """for item in sequences
+                   for v, e, p in 1..1 any item._id graph "Simple"
+                       filter e.question_3 == @q3
+                       return p""",
+            bind_vars={'q3': q3}
+        )
+
+    else:
+        cursor = database.aql.execute(
+            """for item in sequences
+                   for v, e, p in 1..1 any item._id graph "Simple"
+                       return p"""
+        )
 
     inter = [doc for doc in cursor]
 
