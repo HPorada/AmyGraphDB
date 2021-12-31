ff8e5153b6a2bc159b4270c887c606deeeb3aa6b
HPorada <HPorada@users.noreply.github.com>
2021-12-31 14:54:15 +0100

    Filter_questions functions corrected

--- management/query_functions/extendedV2_queries.py
+++ management/query_functions/extendedV2_queries.py
@@ -2,12 +2,17 @@ import json
 
 
 def filter_questions_extendedV2(database, q1, q2, q3, filename="result", directory=None):
-    q1 = "question1/" + q1.replace(".", "").replace(",", "").replace(";", "").replace(" ", "_")
-    q2 = "question2/" + q2.replace(".", "").replace(",", "").replace(";", "").replace(" ", "_")
-    q3 = "question3/" + q3.replace(".", "").replace(",", "").replace(";", "").replace(" ", "_")
+    if (
+            q1.lower() == "faster_aggregation" or q1.lower() == "slower_aggregation" or q1.lower() == "no_aggregation" or q1.lower() == "no_effect" or q1.lower() == "no_information") and (
+            q2.lower() == "yes_direct_evidence" or q2.lower() == "yes_implied_by_kinetics" or q2.lower() == "formation_of_fibrils_by_the_interactee_is_inhibited" or q2.lower() == "no" or q2.lower() == "no_information") and (
+            q3.lower() == "yes" or q3.lower() == "no" or q3.lower() == "no_information"):
 
-    cursor = database.aql.execute(
-        """let ints = (
+        q1 = "question1/" + q1
+        q2 = "question2/" + q2
+        q3 = "question3/" + q3
+
+        cursor = database.aql.execute(
+            """let ints = (
                 for i in interactionsE
                     let q1 = (
                         for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
@@ -34,8 +39,197 @@ def filter_questions_extendedV2(database, q1, q2, q3, filename="result", directo
             for i in ints
                 for v, e, p in 1..1 any i.interactions._id graph "ExtendedV2"
                     return p""",
-        bind_vars={'q1': q1, 'q2': q2, 'q3': q3}
-    )
+            bind_vars={'q1': q1, 'q2': q2, 'q3': q3}
+        )
+
+    elif (
+            q1.lower() == "faster_aggregation" or q1.lower() == "slower_aggregation" or q1.lower() == "no_aggregation" or q1.lower() == "no_effect" or q1.lower() == "no_information") and (
+            q2.lower() == "yes_direct_evidence" or q2.lower() == "yes_implied_by_kinetics" or q2.lower() == "formation_of_fibrils_by_the_interactee_is_inhibited" or q2.lower() == "no" or q2.lower() == "no_information"):
+
+        q1 = "question1/" + q1
+        q2 = "question2/" + q2
+
+        cursor = database.aql.execute(
+            """let ints = (
+                for i in interactionsE
+                    let q1 = (
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q1
+                            return {"interactions": i}
+                    )
+                    let q2 = (
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q2
+                            return {"interactions": i}
+                    )
+                
+                let final = intersection(q1, q2)
+                
+                for item in final 
+                    return {"interactions": item.interactions}
+            )
+                    
+            for i in ints
+                for v, e, p in 1..1 any i.interactions._id graph "ExtendedV2"
+                    return p""",
+            bind_vars={'q1': q1, 'q2': q2}
+        )
+
+    elif (
+            q2.lower() == "yes_direct_evidence" or q2.lower() == "yes_implied_by_kinetics" or q2.lower() == "formation_of_fibrils_by_the_interactee_is_inhibited" or q2.lower() == "no" or q2.lower() == "no_information") and (
+            q3.lower() == "yes" or q3.lower() == "no" or q3.lower() == "no_information"):
+
+        q2 = "question2/" + q2
+        q3 = "question3/" + q3
+
+        cursor = database.aql.execute(
+            """let ints = (
+                for i in interactionsE
+                    let q2 = (
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q2
+                            return {"interactions": i}
+                    )
+                    let q3 = (  
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q3
+                            return {"interactions": i}
+                    )
+                
+                let final = intersection(q2, q3)
+                
+                for item in final 
+                    return {"interactions": item.interactions}
+            )
+                    
+            for i in ints
+                for v, e, p in 1..1 any i.interactions._id graph "ExtendedV2"
+                    return p""",
+            bind_vars={'q2': q2, 'q3': q3}
+        )
+
+    elif (
+            q1.lower() == "faster_aggregation" or q1.lower() == "slower_aggregation" or q1.lower() == "no_aggregation" or q1.lower() == "no_effect" or q1.lower() == "no_information") and (
+            q3.lower() == "yes" or q3.lower() == "no" or q3.lower() == "no_information"):
+
+        q1 = "question1/" + q1
+        q3 = "question3/" + q3
+
+        cursor = database.aql.execute(
+            """let ints = (
+                for i in interactionsE
+                    let q1 = (
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q1
+                            return {"interactions": i}
+                    )
+                    let q3 = (  
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q3
+                            return {"interactions": i}
+                    )
+                
+                let final = intersection(q1, q3)
+                
+                for item in final 
+                    return {"interactions": item.interactions}
+            )
+                    
+            for i in ints
+                for v, e, p in 1..1 any i.interactions._id graph "ExtendedV2"
+                    return p""",
+            bind_vars={'q1': q1, 'q3': q3}
+        )
+
+    elif (
+            q1.lower() == "faster_aggregation" or q1.lower() == "slower_aggregation" or q1.lower() == "no_aggregation" or q1.lower() == "no_effect" or q1.lower() == "no_information"):
+
+        q1 = "question1/" + q1
+
+        cursor = database.aql.execute(
+            """let ints = (
+                for i in interactionsE
+                    let q1 = (
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q1
+                            return {"interactions": i}
+                    )
+                                    
+                for item in q1 
+                    return {"interactions": item.interactions}
+            )
+                    
+            for i in ints
+                for v, e, p in 1..1 any i.interactions._id graph "ExtendedV2"
+                    return p""",
+            bind_vars={'q1': q1}
+        )
+
+    elif (
+            q2.lower() == "yes_direct_evidence" or q2.lower() == "yes_implied_by_kinetics" or q2.lower() == "formation_of_fibrils_by_the_interactee_is_inhibited" or q2.lower() == "no" or q2.lower() == "no_information"):
+
+        q2 = "question2/" + q2
+
+        cursor = database.aql.execute(
+            """let ints = (
+                for i in interactionsE
+                    let q2 = (
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q2
+                            return {"interactions": i}
+                    )
+                                    
+                for item in q2 
+                    return {"interactions": item.interactions}
+            )
+                    
+            for i in ints
+                for v, e, p in 1..1 any i.interactions._id graph "ExtendedV2"
+                    return p""",
+            bind_vars={'q2': q2}
+        )
+
+    elif (
+            q3.lower() == "yes" or q3.lower() == "no" or q3.lower() == "no_information"):
+
+        q3 = "question3/" + q3
+
+        cursor = database.aql.execute(
+            """let ints = (
+                for i in interactionsE
+                    let q3 = (
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            filter v._id == @q3
+                            return {"interactions": i}
+                    )
+                                    
+                for item in q3 
+                    return {"interactions": item.interactions}
+            )
+                    
+            for i in ints
+                for v, e, p in 1..1 any i.interactions._id graph "ExtendedV2"
+                    return p""",
+            bind_vars={'q3': q3}
+        )
+
+    else:
+        cursor = database.aql.execute(
+            """let ints = (
+                for i in interactionsE
+                    let q = (
+                        for v, e, p in 1..1 outbound i._id graph "ExtendedV2"
+                            return {"interactions": i}
+                    )
+                                    
+                for item in q 
+                    return {"interactions": item.interactions}
+            )
+                    
+            for i in ints
+                for v, e, p in 1..1 any i.interactions._id graph "ExtendedV2"
+                    return p"""
+        )
 
     inter = [doc for doc in cursor]
 
